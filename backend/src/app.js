e9414e376a9994235431a7631a0bea34ba9d352e
Risha Mondal <rishamondal328@gmail.com>
2025-09-21 20:30:08 +0530

    first commit

---
+++ backend/src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(rateLimit({ windowMs: 1 * 60 * 1000, max: 100 }));
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Expirable URL API Running 🚀");
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
