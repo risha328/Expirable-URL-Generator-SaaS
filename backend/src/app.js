36fd3a5a8314fea1347d0f7ff21fccf297e134e0
Risha Mondal <rishamondal328@gmail.com>
2025-10-30 21:45:03 +0530

    isuue fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,9 @@ const app = express();
 
 // Middleware
 app.use(helmet());
-app.use(cors());
+app.use(cors(
+  { origin: process.env.BASE_URL || "http://localhost:5001" }
+));
 // app.use(rateLimit({ windowMs: 1 * 60 * 1000, max: 100 })); // Disabled for development
 
 // Use express.json() for all requests
@@ -36,3 +38,6 @@ app.use("/chat", chatbotRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
